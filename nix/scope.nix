5de9949f945702ae69b330c311bbd9ce16443043
Shun Ueda <me@shu.nu>
2026-01-07 10:31:11 -0500

    docs: update comments

--- nix/scope.nix
+++ nix/scope.nix
@@ -15,7 +15,7 @@
           scope = lib.makeScope pkgs.newScope (scopeSelf: {
             inherit (pkgs) terraform;
 
-            # Injecting self as terranix-modules, to all the tests to make tests look like how the
+            # Injecting self as terranix-modules to all the tests so the tests look like how the
             # users would use.
             terranix-modules = self;
 
